6d6c8004604db766b41c7a2b92058325101ec01e
lenchi <enzoleonelsosa98@gmail.com>
2022-07-18 19:47:13 -0300

    inicial

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-let nombre = "enzo leonel ";
-let apellido = 'sosa '
-let saludo = 'Les doy la bienvenida ';
-let fraseCompleta = saludo + nombre + apellido;
-console.log(fraseCompleta);
+// let nombre = "enzo leonel ";
+// let apellido = 'sosa '
+// let saludo = 'Les doy la bienvenida ';
+// let fraseCompleta = saludo + nombre + apellido;
+// console.log(fraseCompleta);
 
-let frase = "JavaScript " + "es un lenguaje " + "esencial para crear " + 'sitios web poderosos.';
-console.log(frase);
-
-
-
-let numeroString1 = "juan 10 ";
-let numeroString2 = "miranda 20 ";
-let numeroString3 = "jose 30 ";
-let concatenaNumeros = numeroString1 + numeroString2 + numeroString3;
-console.log(concatenaNumeros);
-
-let age = prompt('cual es tu edad?');
-console.log(age);
+// let frase = "JavaScript " + "es un lenguaje " + "esencial para crear " + 'sitios web poderosos.';
+// console.log(frase);
 
 
 
-sexo = prompt('introduce tu sexo');
+// let numeroString1 = "juan 10 ";
+// let numeroString2 = "miranda 20 ";
+// let numeroString3 = "jose 30 ";
+// let concatenaNumeros = numeroString1 + numeroString2 + numeroString3;
+// console.log(concatenaNumeros);
+
+// let age = prompt('cual es tu edad?');
+// console.log(age);
+
+
+
+let sexo = prompt('introduce tu sexo');
 
 if (sexo == 'masculino'){
     alert('podes entrar si sos mayor de 18');
 
-    edad = prompt("Introduce tu edad");
+   let edad = prompt("Introduce tu edad");
     if (edad >= 18){
         alert("Puedes entrar, eres mayor de edad.");
        } else {
@@ -36,7 +36,7 @@ if (sexo == 'masculino'){
 else if(sexo == 'femenino'){
     alert('mujeres solo mayores de 20');
 
-    edad = prompt("Introduce tu edad");
+    let edad = prompt("Introduce tu edad");
     if (edad >= 20){
         alert("Puedes entrar, eres mayor de edad.");
        } else {
@@ -45,7 +45,5 @@ else if(sexo == 'femenino'){
 }else{
     alert('no has introducido tu sexo correctamente')
 }
-
-
 
 
